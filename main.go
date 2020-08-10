655b0daedc3074f667269eef54a8917972f14e7a
David Solomon <davidsolomon@Davids-iMac.local>
2020-08-10 16:15:57 -0700

    Created a Signup Page

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 var (
 	homeView    *views.View
 	contactView *views.View
+	signupView  *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -24,15 +25,21 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	must(contactView.Render(w, nil))
 }
 
-func main() {
-	fmt.Println("begin main")
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signupView.Render(w, nil))
+}
 
-	homeView = views.NewView("bootstrap","views/home.gohtml")
+func main() {
+	fmt.Println("starting main")
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	signupView = views.NewView("bootstrap", "views/signup.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/signup", signup)
 
 	http.ListenAndServe(":3000", r)
 }
